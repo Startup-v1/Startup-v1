5f1aca1eb5d2f98f5aa0fcbe6fce682e5e1ea85c
AdrianDanlos <danlosadrian@gmail.com>
2024-03-18 01:55:24 +0800

    Test landing page created

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,44 @@
+import video from "./assets/video.mp4";
+
 function App() {
   return (
-    <button className="inline-block cursor-pointer rounded-md bg-gray-800 px-4 py-3 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-900">
-      HELLO WORLD
-    </button>
+    <>
+      <div className="hero-video-container">
+        <div className="curtain"></div>
+        <video
+          className="hero-video"
+          autoPlay
+          loop
+          muted
+          playsInline
+          preload="auto"
+        >
+          <source src={video} type="video/mp4"></source>
+          Your browser does not support HTML5 video.
+        </video>
+      </div>
+      <div className="hero-content">
+        <h1>
+          <div>Work Remote.</div>
+          <div>Live Local.</div>
+          <div>Go Nomad!</div>
+        </h1>
+        <section>
+          <div className="font-bold text-primary">
+            Trusted and reliable data. Join our fresh and growing community and
+            live your adventure!
+          </div>
+          <div>
+            Connect with like-minded peers through meetups & activities.
+          </div>
+          <div>Discover the ideal place to settle down.</div>
+          <div>
+            Track your journeys and show the world the places you've visited.
+          </div>
+          <button className="btn btn-primary mt-8">Sign up</button>
+        </section>
+      </div>
+    </>
   );
 }
 
