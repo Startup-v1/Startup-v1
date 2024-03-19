7eb9ff0202bd6c4930c40ffa9049fbf38131119c
massije <jedc.marti@gmail.com>
2024-03-19 11:53:04 +0100

    Create-top-level-componenets-Finished

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './scss/index.scss'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./scss/index.scss";
+import { HomePage } from "./toplevelcomponents/homePage/homePage.tsx";
+import { ProfilePage } from "./toplevelcomponents/profilePage/profilePage.tsx";
+import { MeetupsPage } from "./toplevelcomponents/meetupsPage/meetupsPage.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   //FIXME: Remove for production?
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <HomePage />
+    <ProfilePage />
+    <MeetupsPage />
+  </React.StrictMode>
+);
