a4aadaf63ddd7f9e62e187b28ddddf2a45c82cf7
Jorge Martin <91974390+massije@users.noreply.github.com>
2024-03-19 12:17:13 +0100

    Navbar-component-Finished (#39)

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,10 +4,12 @@ import "./scss/index.scss";
 import { HomePage } from "./toplevelcomponents/homePage/homePage.tsx";
 import { ProfilePage } from "./toplevelcomponents/profilePage/profilePage.tsx";
 import { MeetupsPage } from "./toplevelcomponents/meetupsPage/meetupsPage.tsx";
+import { Navbar } from "./components/navbar.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   //FIXME: Remove for production?
   <React.StrictMode>
+    <Navbar />
     <HomePage />
     <ProfilePage />
     <MeetupsPage />
