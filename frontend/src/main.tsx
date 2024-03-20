3ab4eb476e819fd71d0a9c4fc576463075b57887
AdrianDanlos <danlosadrian@gmail.com>
2024-03-20 20:14:26 +0800

    test

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,7 +6,7 @@ import { RouterProvider } from "react-router-dom";
 import { router } from "./routes/routes.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  //FIXME: Remove for production?
+  //FIXME: Removee for production?
   <React.StrictMode>
     <Navbar />
     <RouterProvider router={router} />
