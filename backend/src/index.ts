02ce21dd3ebcc6cf9a9425052f96527b7d26bef8
Joel Encinas <joelencinasmartin@gmail.com>
2024-03-20 16:24:49 +0100

    45 set up typescript development compiler in backend (#50)
    * create test route and run ts-node
    
    * add nodemon as dependency + commands
    
    * add an additional route to test nodemon
    
    ---------
    
    Co-authored-by: Adrian Danlos <danlosadrian@gmail.com>

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, {Request, Response} from "express";
 import mongoose, { ConnectOptions } from "mongoose";
 
 const app = express();
@@ -7,10 +7,20 @@ const port = process.env.PORT || 3000;
 //FIXME: Replace with db uri
 const dbUri = "test";
 
+/*
 mongoose.connect(dbUri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 } as ConnectOptions);
+*/
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!')
+})
+
+app.get('/hello', (req: Request, res: Response) => {
+  res.send('Hello there!')
+})
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
