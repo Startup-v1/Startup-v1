0c9513ee27cd06ecaa0f57f6e4c1e5428d407c2c
Jorge Martin <91974390+massije@users.noreply.github.com>
2024-03-20 17:27:20 +0100

    Navbar-with-routes-established (#51)

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -1,25 +1,49 @@
+import { Link } from "react-router-dom";
+import { RoutePaths } from "../routes/routePaths";
+
 export function Navbar() {
   return (
     <div className="navbar bg-base-100">
       <div className="flex-1">
         <a className="btn btn-ghost text-xl">daisyUI</a>
       </div>
+
       <div className="flex-none">
-        <button className="btn btn-square btn-ghost">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            className="inline-block w-5 h-5 stroke-current"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
-            ></path>
-          </svg>
-        </button>
+        <ul className="menu menu-horizontal px-1">
+          <li>
+            <Link to={RoutePaths.index}>Discover</Link>
+          </li>
+
+          <li>{/* <Link to="/meetups">Meetups</Link> */}</li>
+        </ul>
+      </div>
+      <div className="dropdown dropdown-end">
+        <div
+          tabIndex={0}
+          role="button"
+          className="btn btn-ghost btn-circle avatar"
+        >
+          <div className="w-10 rounded-full">
+            <img
+              alt="Tailwind CSS Navbar component"
+              src="https://adriandanlos.es/images/me.png"
+            />
+          </div>
+        </div>
+        <ul
+          tabIndex={0}
+          className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52"
+        >
+          <li>
+            <Link to={RoutePaths.profile}>Profile</Link>
+          </li>
+          <li>
+            <Link to="/">Settings</Link>
+          </li>
+          <li>
+            <Link to="/">Logout</Link>
+          </li>
+        </ul>
       </div>
     </div>
   );
