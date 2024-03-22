c8c19fa3390f27f768f2b57979f53c758a7a7453
Joel Encinas <joelencinasmartin@gmail.com>
2024-03-22 11:24:02 +0100

    test db (#70)
    bug to production

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,10 @@
 import express, {Request, Response} from "express";
 import mongoose, { ConnectOptions } from "mongoose";
+import * as dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
-
-//FIXME: Replacee with db uri
-const dbUri = "test";
 
 /*
 mongoose.connect(dbUri, {
@@ -21,6 +20,14 @@ app.get('/', (req: Request, res: Response) => {
 app.get('/hello', (req: Request, res: Response) => {
   res.send('Hello there!')
 })
+
+//FIXME: Replace with db uri
+const uri: any = process.env.DB_URL;
+
+mongoose
+  .connect(uri)
+  .then(() => console.log("Connected to DB"))
+  .catch(console.error);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
