85816634c0b6059f5e1b07237c9f16b20a2a3c92
JoelEncinas <joelencinasmartin@gmail.com>
2024-03-22 16:52:33 +0100

    connect to mongodb atlas

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,33 +1,28 @@
-import express, {Request, Response} from "express";
+import express, { Request, Response } from "express";
 import mongoose, { ConnectOptions } from "mongoose";
 import * as dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
+const dbUri: any = process.env.DB_URL;
 
-/*
-mongoose.connect(dbUri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-} as ConnectOptions);
-*/
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World!");
+});
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!')
-})
-
-app.get('/hello', (req: Request, res: Response) => {
-  res.send('Hello there!')
-})
-
-//FIXME: Replace with db uri
-const uri: any = process.env.DB_URL;
+app.get("/hello", (req: Request, res: Response) => {
+  res.send("Hello there!");
+});
 
 mongoose
-  .connect(uri)
-  .then(() => console.log("Connected to DB"))
-  .catch(console.error);
+  .connect(dbUri)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB:", err);
+  });
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
