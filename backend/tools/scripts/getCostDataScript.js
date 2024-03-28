922c5790e69763db931b17f07a9f62faf05e7c3d
JoelEncinas <joelencinasmartin@gmail.com>
2024-03-28 19:35:41 +0100

    test script for first city

--- backend/tools/scripts/getCostDataScript.js
+++ backend/tools/scripts/getCostDataScript.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import cheerio from "cheerio";
+import initialCities from "../initialCities.js";
 
 async function scrapeData(url) {
   try {
@@ -20,10 +21,18 @@ async function scrapeData(url) {
   }
 }
 
-const url = "https://www.numbeo.com/cost-of-living/in/New-York";
-
 (async () => {
   try {
+    let firstItem;
+
+    // test for first city
+    for (const item of initialCities) {
+      firstItem = item;
+      break; // Exit the loop after the first iteration
+    }
+
+    const url = `https://www.numbeo.com/cost-of-living/in/${firstItem}`;
+
     const scrapedData = await scrapeData(url);
     console.log("Price:", scrapedData);
   } catch (error) {
