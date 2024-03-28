e12c8bee45fdbe7cf4c177eb8b19b54638b6687e
JoelEncinas <joelencinasmartin@gmail.com>
2024-03-28 19:15:31 +0100

    extracted price for 1 month rent in city centre

--- backend/tools/scripts/getCostDataScript.js
+++ backend/tools/scripts/getCostDataScript.js
@@ -1,22 +1,32 @@
 import axios from "axios";
+import cheerio from "cheerio";
 
 async function scrapeData(url) {
-    try {
-        const response = await axios.get(url);
-        const htmlContent = response.data;
-        console.log(htmlContent);
+  try {
+    const response = await axios.get(url);
+    const htmlContent = response.data;
 
-    } catch (error) {
-        console.error('Error:', error);
-    }
+    const $ = cheerio.load(htmlContent);
+
+    const apartmentData = $(
+      'td:contains("Apartment (1 bedroom) in City Centre")'
+    ).next();
+
+    const price = apartmentData.text().trim();
+
+    return price;
+  } catch (error) {
+    console.error("Error:", error);
+  }
 }
 
-const url = 'https://www.numbeo.com/cost-of-living/in/New-York';
+const url = "https://www.numbeo.com/cost-of-living/in/New-York";
 
-scrapeData(url)
-    .then((data) => {
-        console.log('Scraped data:', data);
-    })
-    .catch((error) => {
-        console.error('Error scraping data:', error);
-    });
+(async () => {
+  try {
+    const scrapedData = await scrapeData(url);
+    console.log("Price:", scrapedData);
+  } catch (error) {
+    console.error("Error scraping data:", error);
+  }
+})();
