2153405fce4e5cf87295b3348c89761a86a3508e
JoelEncinas <joelencinasmartin@gmail.com>
2024-04-22 11:28:23 +0200

    save monthly cost and apartment, add visual to requests

--- backend/tools/scripts/getCostDataScript.js
+++ backend/tools/scripts/getCostDataScript.js
@@ -17,9 +17,23 @@ async function scrapeData(url) {
       'td:contains("Average Monthly Net Salary (After Tax)")'
     ).next();
 
-    const price = apartmentData.text().trim();
+    const monthlyCostData = $(
+      'li:contains("A single person estimated monthly costs are")'
+    ).children();
 
-    return price;
+    const apartmentDataPrice = apartmentData.text().trim();
+    const apartmentDataPriceTrim = apartmentDataPrice.replace(/\u00A0/g, "");
+    const monthlyCostDataPrice = monthlyCostData.text().trim();
+    const monthlyCostDataPriceTrim = monthlyCostDataPrice
+      .substring(0, monthlyCostDataPrice.indexOf("("))
+      .trim();
+
+    const cityObject = {
+      apartment: apartmentDataPriceTrim,
+      monthlyCost: monthlyCostDataPriceTrim,
+    };
+
+    return cityObject;
   } catch (error) {
     console.error("Error:", error);
   }
@@ -47,14 +61,17 @@ const saveFile = (data, missing) => {
 
     for (const city of initialCities) {
       currentCity = city.split(" ").join("-");
-      const url = `https://www.numbeo.com/cost-of-living/in/${currentCity}`;
+      const url = `https://www.numbeo.com/cost-of-living/in/${currentCity}?displayCurrency=USD`;
       scrapedData = await scrapeData(url);
 
-      if (scrapedData === "") {
+      if (scrapedData.apartment === "") {
         citiesCostOfLiving[city] = "no data";
         citiesMissing++;
+        console.log(`${city} ❌`);
       } else {
-        citiesCostOfLiving[city] = scrapedData.replace(/\u00A0/g, "");
+        console.log(`${city} ✅`);
+
+        citiesCostOfLiving[city] = scrapedData;
       }
       await new Promise((resolve) => setTimeout(resolve, timeBetweenRequests));
     }
