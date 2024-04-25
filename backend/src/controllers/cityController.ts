13d3c9304fa66905dc09fe19ddc5f4e9d91a3ba3
AdrianDanlos <danlosadrian@gmail.com>
2024-04-25 21:30:53 +0800

    city route created

---
+++ backend/src/controllers/cityController.ts
@@ -0,0 +1,18 @@
+//TODO: Should we use lean?
+import { Request, Response } from "express";
+const City = require("../models/City");
+
+export const getCities = async (req: Request, res: Response) => {
+  try {
+    const cities = await City.find();
+
+    if (cities?.length > 0) {
+      return res.status(200).json(cities);
+    }
+
+    return res.status(404).json({ message: "No cities found" });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ message: 'Error' });
+  }
+};
