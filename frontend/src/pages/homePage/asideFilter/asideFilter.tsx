feb6eb503fc7a6e665916a4c7a53e7463f114549
massije <jedc.marti@gmail.com>
2024-04-29 11:25:03 +0200

    aside-Filter-mobile

--- frontend/src/pages/homePage/asideFilter/asideFilter.tsx
+++ frontend/src/pages/homePage/asideFilter/asideFilter.tsx
@@ -2,14 +2,32 @@ import "./asideFilter.scss";
 
 export function AsideFilter() {
   return (
-    <div className="artboard phone-4 artboard-me">
-      <div className="filter-begin">
-        <div className="filters-begin-end-divider">
-          <div className="flex-1">
-            <a className="btn btn-ghost text-xl">daisyUI</a>
-          </div>
-          <label className="input input-bordered flex items-center gap-2 h-8">
-            <input type="text" className="grow text-xs" placeholder="Search" />
+    <div className="">
+      <div className="w-[360px] h-screen flex flex-col border-r border-gray-300">
+        {/* first block */}
+        <div className="p-4 pb-1 flex flex-items-center justify-between ">
+          {/* close button */}
+          <button className="btn btn-square border border-gray-200">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </svg>
+          </button>
+
+          {/* clear button */}
+          {/* <button className="btn">Clear filters</button> */}
+          <label className="input input-bordered flex items-center gap-2">
+            <input type="text" className="grow" placeholder="Filters" />
             <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 16 16"
@@ -24,172 +42,194 @@ export function AsideFilter() {
             </svg>
           </label>
         </div>
-      </div>
-      <div className="all-filters">
-        <div className="filter filter-item filter-item-1">
-          <div className="filter-name">Weather</div>
-          <div className="container-filter-items">
+
+        {/* test-clean-filters */}
+        <div className="px-4 py-2 border-b border-gray-300">
+          {/* clear button */}
+          {/* <button className="btn btn-sm">Clear filters</button> */}
+          <button className="btn btn-block btn-sm border border-gray-200">
+            Clear filters
+          </button>
+        </div>
+
+        {/* second block */}
+        <div className="p-4">
+          <p className="font-bold pb-2">Wheather</p>
+          <div className="flex flex-wrap gap-3 pt-1">
             <input
               type="checkbox"
+              name="weather"
               aria-label="☀️ Warm now"
-              className="btn btn-sm custom-checkbox"
+              className="btn btn-sm border border-gray-200"
             />
             <input
               type="checkbox"
+              name="weather"
               aria-label="⛅ Mild now"
-              className="btn btn-sm custom-checkbox"
+              className="btn btn-sm border border-gray-200"
             />
             <input
               type="checkbox"
+              name="weather"
               aria-label="☁️ Cold now"
-              className="btn btn-sm custom-checkbox"
+              className="btn btn-sm border border-gray-200"
             />
             <input
               type="checkbox"
+              name="weather"
               aria-label="❄️ Snow now"
-              className="btn btn-sm custom-checkbox"
+              className="btn btn-sm border border-gray-200"
             />
           </div>
         </div>
-        <hr className="divider-filter-groups" />
-        <div className="filter filter-item filter-item-2">
-          <div className="filter-name filter-name-where">Where</div>
-          <div className="container-filter-items">
-            <div className="join continent-item">
-              <input
-                className="btn continent-btn"
-                type="radio"
-                name="options"
-                aria-label="⛅ North America"
-              />
-              <input
-                className="btn continent-btn"
-                type="radio"
-                name="options"
-                aria-label="⛅Europe"
-              />
-              <input
-                className="btn continent-btn"
-                type="radio"
-                name="options"
-                aria-label="⛅ Asia"
-              />
-              <input
-                className="btn continent-btn"
-                type="radio"
-                name="options"
-                aria-label="⛅ Latin America"
-              />
 
-              <input
-                className="btn continent-btn"
-                type="radio"
-                name="options"
-                aria-label="⛅ Africa"
-              />
-
-              <input
-                className="btn continent-btn"
-                type="radio"
-                name="options"
-                aria-label="⛅ Antartida"
-              />
-              <input
-                className="btn continent-btn"
-                type="radio"
-                name="options"
-                aria-label="⛅ Oceania"
-              />
-            </div>
-          </div>
-        </div>
-        <hr className="divider-filter-groups" />
-        <div className="filter filter-item filter-item-3">
-          <div className="filter-name filter-name-price">Price</div>
-          <div className="container-filter-items">
-            <div className="container-price-items">
-              <label className=" w-full max-w-xs centered-label">
-                <select className="select w-full max-w-xs select-bordered custom-select">
-                  <option disabled selected>
-                    Pick one
-                  </option>
-                  <option>200 €</option>
-                  <option>300 €</option>
-                  <option>400 €</option>
-                  <option>500 €</option>
-                  <option>99999 €</option>
-                </select>
-                <div className="label">
-                  <span className="label-text-alt">min</span>
-                </div>
-              </label>
-              <label className=" w-full max-w-xs centered-label">
-                <select className="select w-full max-w-xs select-bordered custom-select">
-                  <option disabled selected>
-                    Pick one
-                  </option>
-                  <option>200 €</option>
-                  <option>300 €</option>
-                  <option>400 €</option>
-                  <option>500 €</option>
-                  <option>99999 €</option>
-                </select>
-                <div className="label">
-                  <span className="label-text-alt">max</span>
-                </div>
-              </label>
-            </div>
-          </div>
-        </div>
-        <hr className="divider-filter-groups" />
-        <div className="filter filter-item filter-item-5">
-          <div className="filter-name filter-name-popular">Popular</div>
-          <div className="container-filter-items">
+        {/* thrid block */}
+        <div className=" p-4">
+          <p className="font-bold pb-2">Where</p>
+          <div className="flex flex-wrap gap-x-3 gap-y-2 pt-1">
             <input
               type="checkbox"
+              name="weather"
+              aria-label="☀️ North America"
+              className="btn border border-gray-200"
+            />
+            <input
+              type="checkbox"
+              name="weather"
+              aria-label="⛅ Europe"
+              className="btn border border-gray-200"
+            />
+            <input
+              type="checkbox"
+              name="weather"
+              aria-label="☁️ Asia"
+              className="btn border border-gray-200"
+            />
+            <input
+              type="checkbox"
+              name="weather"
+              aria-label="❄️ Latin America"
+              className="btn border border-gray-200"
+            />
+            <input
+              type="checkbox"
+              name="weather"
+              aria-label="⛅ Africa"
+              className="btn border border-gray-200"
+            />
+            <input
+              type="checkbox"
+              name="weather"
+              aria-label="☁️ Antartida"
+              className="btn border border-gray-200"
+            />
+            <input
+              type="checkbox"
+              name="weather"
+              aria-label="❄️ Oceania"
+              className="btn border border-gray-200"
+            />
+          </div>
+        </div>
+
+        {/* Four block */}
+        <div className="p-4">
+          <p className="font-bold pb-2">Price</p>
+          <div className="flex gap-10">
+            <label className="">
+              <select className="select border border-gray-300">
+                <option disabled selected>
+                  Pick one
+                </option>
+                <option>200 €</option>
+                <option>300 €</option>
+                <option>400 €</option>
+                <option>500 €</option>
+                <option>99999 €</option>
+              </select>
+              <div className="label">
+                <span className="label-text-alt">min</span>
+              </div>
+            </label>
+            <label className="">
+              <select className="select border border-gray-300">
+                <option disabled selected>
+                  Pick one
+                </option>
+                <option>200 €</option>
+                <option>300 €</option>
+                <option>400 €</option>
+                <option>500 €</option>
+                <option>99999 €</option>
+              </select>
+              <div className="label">
+                <span className="label-text-alt">max</span>
+              </div>
+            </label>
+          </div>
+        </div>
+
+        {/* fifth block */}
+        <div className="p-4">
+          <p className="font-bold pb-2">Wheather</p>
+          <div className="flex flex-wrap gap-3 pt-1">
+            <input
+              type="checkbox"
+              name="weather"
               aria-label="☀️ Safe"
-              className="btn btn-sm custom-checkbox"
+              className="btn btn-sm border border-gray-200"
             />
             <input
               type="checkbox"
+              name="weather"
               aria-label="⛅ Fast internet"
-              className="btn btn-sm custom-checkbox"
+              className="btn btn-sm border border-gray-200"
             />
             <input
               type="checkbox"
+              name="weather"
               aria-label="☁️ Clean air now"
-              className="btn btn-sm custom-checkbox"
+              className="btn btn-sm border border-gray-200"
             />
             <input
               type="checkbox"
+              name="weather"
               aria-label="❄️ Snow now"
-              className="btn btn-sm custom-checkbox"
+              className="btn btn-sm border border-gray-200"
             />
             <input
               type="checkbox"
+              name="weather"
+              aria-label="☀️ Safe"
+              className="btn btn-sm border border-gray-200"
+            />
+            <input
+              type="checkbox"
+              name="weather"
+              aria-label="⛅ Fast internet"
+              className="btn btn-sm border border-gray-200"
+            />
+            <input
+              type="checkbox"
+              name="weather"
+              aria-label="☁️ Clean air now"
+              className="btn btn-sm border border-gray-200"
+            />
+            <input
+              type="checkbox"
+              name="weather"
               aria-label="❄️ Snow now"
-              className="btn btn-sm custom-checkbox"
-            />
-            <input
-              type="checkbox"
-              aria-label="❄️ Snow now"
-              className="btn btn-sm custom-checkbox"
-            />
-            <input
-              type="checkbox"
-              aria-label="❄️ Snow now"
-              className="btn btn-sm custom-checkbox"
-            />
-            <input
-              type="checkbox"
-              aria-label="❄️ Top tinder"
-              className="btn btn-sm custom-checkbox"
+              className="btn btn-sm border border-gray-200"
             />
           </div>
         </div>
-      </div>
-      <div className="filter-end">
-        <button className="btn btn-active btn-ghost btn-sm">Clear all</button>
+
+        {/* Sixth block */}
+        <div className="p-4 border-t border-gray-300">
+          <button className="btn btn-block border border-gray-300">
+            Apply filters
+          </button>
+        </div>
       </div>
     </div>
   );
