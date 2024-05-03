bc61419ca567e6275c1847371cc1cd8e5c373d2a
AdrianDanlos <danlosadrian@gmail.com>
2024-05-03 22:24:08 +0800

    import fixes

--- backend/src/controllers/cityController.ts
+++ backend/src/controllers/cityController.ts
@@ -1,12 +1,14 @@
 //TODO: Should we use lean?
 import { Request, Response } from "express";
-const City = require("../models/City");
+import City from "../models/City";
 
 export const getCities = async (req: Request, res: Response) => {
   try {
     //FIXME: SET A HIGHER LIMIT
     const cities = await City.find()
-      .select("name weather population photoUrl.small country.name country.safetyIndex")
+      .select(
+        "name weather population photoUrl.small country.name country.safetyIndex"
+      )
       .limit(15);
 
     if (cities?.length > 0) {
