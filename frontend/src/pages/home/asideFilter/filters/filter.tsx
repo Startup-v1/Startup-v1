6651f36045dc0a858582c03d1c125a472f2e6f7c
Jorge Martin <91974390+massije@users.noreply.github.com>
2024-05-15 18:15:17 +0200

    Clean-up (#197)

--- frontend/src/pages/home/asideFilter/filters/filter.tsx
+++ frontend/src/pages/home/asideFilter/filters/filter.tsx
@@ -1,15 +1,28 @@
+import { useState } from "react";
+
 type Props = {
   label: string;
   sizeButton?: string;
 };
 
 export function Filter({ label, sizeButton }: Props) {
+  // Estado para controlar si el botón está presionado o no
+  const [presionado, setPresionado] = useState(false);
+
+  // Función para manejar el evento de clic en el botón
+  const handleClick = () => {
+    setPresionado(!presionado); // Cambiar el estado de presionado al contrario de su estado actual
+  };
+
   return (
-    <input
-      type="checkbox"
-      name="weather"
-      aria-label={label}
-      className={`btn ${sizeButton} border border-gray-200`}
-    />
+    <button
+      type="submit"
+      className={`rounded-lg inline-block px-1 py-1 ${sizeButton} border border-gray-200 ${
+        presionado ? "bg-blue-500 text-white" : ""
+      }`}
+      onClick={handleClick} // Asignar la función handleClick al evento onClick del botón
+    >
+      {label}
+    </button>
   );
 }
