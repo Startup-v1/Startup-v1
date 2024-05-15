f3360b3bef95f222ea05e253a3bb295a3a3c33b6
Jorge Martin <91974390+massije@users.noreply.github.com>
2024-05-15 17:08:06 +0200

    Sticky effect established (#195)
    * aside-Filter-mobile
    
    cities-fixed
    
    aside-filters-size-fixed
    
    aside-filters-is-Sticky
    
    sticky applied to non-filter buttons
    
    sticky applied to non-filter buttons
    
    organized sideFilter
    
    Applied price range filter logic
    
    thumbs-space-fixed
    
    Hidden effect applied finished
    
    Fan-out concealment effect applied
    
    Hidden effect resolved and set to the right
    
    Sticky effect established
    
    * Clean-up

---
+++ frontend/src/pages/home/asideFilter/filterOptions.ts
@@ -0,0 +1,24 @@
+// filterOptions.ts
+
+// Agrupar los arrays en un objeto
+const filterOptions = {
+  weather: ["☁️ Cold now", "⛅ Europe", "☁️ Cold now", "⛅ Europe"],
+  where: [
+    "☁️ North America",
+    "☁️ Asia",
+    "❄️ Latin America",
+    "⛅ Europe",
+    "⛅ Africa",
+    "☁️ Antartida",
+    "❄️ Oceania",
+  ],
+  other: [
+    "☁️ Safe",
+    "☁️ Fast internet",
+    "⛅ Clean air now",
+    "❄️ Snow now",
+    "⛅ Africa",
+  ],
+};
+
+export default filterOptions;
---
+++ frontend/src/pages/home/asideFilter/filters/filterPriceRange/filterPriceRange.tsx
@@ -0,0 +1,95 @@
+import "./filterPriceRange.scss";
+import Slider from "react-slider";
+import { useState } from "react";
+import CurrencyInput from "react-currency-input-field";
+
+const MIN = 0;
+const MAX = 10000;
+
+export function FilterPriceRange() {
+  const [values, setValues] = useState([MIN, MAX]);
+  const [isMinInputEmpty, setIsMinInputEmpty] = useState(false);
+  const [isMaxInputEmpty, setIsMaxInputEmpty] = useState(false);
+
+  const handleMinInputChange = (value: string | undefined) => {
+    if (!value || parseInt(value.toString()) === 0) {
+      setValues([MIN, values[1] > MAX ? MAX : values[1]]);
+      setIsMinInputEmpty(true);
+      return;
+    }
+    setIsMinInputEmpty(false);
+    const parsedValue = parseInt(value!.replace(/[^\d]/g, ""));
+    setValues([parsedValue, values[1] > parsedValue ? values[1] : parsedValue]);
+  };
+
+  const handleMaxInputChange = (value: string | undefined) => {
+    if (!value || parseInt(value.toString()) === 0) {
+      setValues([values[0] < MIN ? MIN : values[0], MAX]);
+      setIsMaxInputEmpty(true);
+      return;
+    }
+    setIsMaxInputEmpty(false);
+    const parsedValue = parseInt(value!.replace(/[^\d]/g, ""));
+    setValues([values[0] < parsedValue ? values[0] : parsedValue, parsedValue]);
+  };
+
+  const handleMinInputFocus = () => {
+    if (isMinInputEmpty) {
+      setValues([0, values[1] > MAX ? MAX : values[1]]);
+    }
+  };
+
+  const handleMaxInputFocus = () => {
+    if (isMaxInputEmpty) {
+      setValues([values[0] < MIN ? MIN : values[0], 0]);
+    }
+  };
+
+  return (
+    <div className="p-4">
+      <p className="font-bold pb-2">Price</p>
+      <div className="app">
+        <Slider
+          className={"slider"}
+          onChange={setValues}
+          value={values}
+          min={MIN}
+          max={MAX}
+        />
+        <div className="price-input">
+          <div className="field">
+            <CurrencyInput
+              className="input-min"
+              value={
+                isMinInputEmpty && values[0] !== null && values[0] !== undefined
+                  ? ""
+                  : values[0].toString()
+              }
+              onValueChange={handleMinInputChange}
+              onFocus={handleMinInputFocus}
+              suffix="€"
+              placeholder="0€"
+              decimalsLimit={0}
+            />
+          </div>
+          <div className="separator"> - </div>
+          <div className="field">
+            <CurrencyInput
+              className="input-max"
+              value={
+                isMaxInputEmpty && values[1] !== null && values[1] !== undefined
+                  ? ""
+                  : values[1].toString()
+              }
+              onValueChange={handleMaxInputChange}
+              onFocus={handleMaxInputFocus}
+              suffix="€"
+              placeholder="10000€"
+              decimalsLimit={0}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
