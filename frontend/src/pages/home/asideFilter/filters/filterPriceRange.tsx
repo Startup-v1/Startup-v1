f093814e8e3e6cb421dc99b59d9d694f3c3e32c8
AdrianDanlos <danlosadrian@gmail.com>
2024-05-18 23:29:07 +0800

    fixed slider

---
+++ frontend/src/pages/home/asideFilter/filters/filterPriceRange.tsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+
+export function FilterPriceRange() {
+  const [value, setValue] = useState("0");
+  return (
+    <div className="p-4">
+      <p className="font-bold pb-2">Price &lt; {value}$</p>
+      <div className="flex flex-wrap gap-3 pt-2">
+        <input
+          type="range"
+          min="500"
+          max="3000"
+          value={value}
+          className="range range-sm"
+          onChange={(e) => {
+            setValue(e.target.value);
+          }}
+        />
+      </div>
+    </div>
+  );
+}
