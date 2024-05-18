fb6171fa01f669b6d28ebf885676f17ab2e89ca7
AdrianDanlos <danlosadrian@gmail.com>
2024-05-18 21:44:29 +0800

    fixed dropdown

--- frontend/src/sharedComponents/navbar/userCurrency.tsx
+++ frontend/src/sharedComponents/navbar/userCurrency.tsx
@@ -1,18 +1,16 @@
 import { useStore } from "@Store/store";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { popularCurrencies } from "src/utils/currencies";
 
 export const UserCurrency = () => {
   const { userCurrency, updateUserCurrency } = useStore();
 
-  const [isOpen, setIsOpen] = useState(false);
-
-  const toggleDropdown = () => {
-    setIsOpen(!isOpen);
-  };
-
-  const closeDropdown = () => {
-    setIsOpen(false);
+  // hack to close daisyUI dropdown when option is chosen https://medium.com/@malikhamzav/how-to-close-daisyui-dropdown-on-click-ea65c5749410
+  const handleClick = () => {
+    const elem: any = document.activeElement;
+    if (elem) {
+      elem?.blur();
+    }
   };
 
   useEffect(() => {
@@ -27,37 +25,30 @@ export const UserCurrency = () => {
 
   return (
     <div className="dropdown dropdown-bottom dropdown-end mr-3">
-      <div
-        onClick={toggleDropdown}
+      <label
         tabIndex={0}
-        role="button"
-        className="btn m-1 p-2 rounded-sm min-h-8 max-h-8 font-normal"
+        className="m-1 btn p-2 rounded-sm min-h-8 max-h-8 font-normal"
       >
         {userCurrency.code}
-      </div>
-      {isOpen && (
-        <ul
-          tabIndex={0}
-          className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52"
-        >
-          {popularCurrencies.map((currency) => (
-            <li key={currency.code}>
-              <a
-                onClick={() => {
-                  closeDropdown();
-                  updateUserCurrency(currency);
-                  localStorage.setItem(
-                    "userCurrency",
-                    JSON.stringify(currency)
-                  );
-                }}
-              >
-                {currency.code} - {currency.symbol}
-              </a>
-            </li>
-          ))}
-        </ul>
-      )}
+      </label>
+      <ul
+        tabIndex={0}
+        className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52"
+      >
+        {popularCurrencies.map((currency) => (
+          <li key={currency.code}>
+            <a
+              onClick={() => {
+                handleClick();
+                updateUserCurrency(currency);
+                localStorage.setItem("userCurrency", JSON.stringify(currency));
+              }}
+            >
+              {currency.code} - {currency.symbol}
+            </a>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
