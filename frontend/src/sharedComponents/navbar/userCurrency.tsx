b9f4abec2f768e4f965ac9b9ca406e04a204215f
AdrianDanlos <danlosadrian@gmail.com>
2024-05-18 18:24:08 +0800

    dropdown logic

--- frontend/src/sharedComponents/navbar/userCurrency.tsx
+++ frontend/src/sharedComponents/navbar/userCurrency.tsx
@@ -1,9 +1,19 @@
 import { useStore } from "@Store/store";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { popularCurrencies } from "src/utils/currencies";
 
 export const UserCurrency = () => {
   const { userCurrency, updateUserCurrency } = useStore();
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const closeDropdown = () => {
+    setIsOpen(false);
+  };
 
   useEffect(() => {
     const userCurrencyFromStorage = JSON.parse(
@@ -17,26 +27,37 @@ export const UserCurrency = () => {
 
   return (
     <div className="dropdown dropdown-bottom dropdown-end mr-3">
-      <div tabIndex={0} role="button" className="btn m-1 p-2 rounded-sm min-h-8 max-h-8 font-normal">
+      <div
+        onClick={toggleDropdown}
+        tabIndex={0}
+        role="button"
+        className="btn m-1 p-2 rounded-sm min-h-8 max-h-8 font-normal"
+      >
         {userCurrency.code}
       </div>
-      <ul
-        tabIndex={0}
-        className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52"
-      >
-        {popularCurrencies.map((currency) => (
-          <li key={currency.code}>
-            <a
-              onClick={() => {
-                updateUserCurrency(currency);
-                localStorage.setItem("userCurrency", JSON.stringify(currency));
-              }}
-            >
-              {currency.code} - {currency.symbol}
-            </a>
-          </li>
-        ))}
-      </ul>
+      {isOpen && (
+        <ul
+          tabIndex={0}
+          className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52"
+        >
+          {popularCurrencies.map((currency) => (
+            <li key={currency.code}>
+              <a
+                onClick={() => {
+                  closeDropdown();
+                  updateUserCurrency(currency);
+                  localStorage.setItem(
+                    "userCurrency",
+                    JSON.stringify(currency)
+                  );
+                }}
+              >
+                {currency.code} - {currency.symbol}
+              </a>
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
