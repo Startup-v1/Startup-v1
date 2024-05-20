f76e797c064f35c4e1c2011f21b104217d8c3dde
massije <jedc.marti@gmail.com>
2024-05-20 14:46:08 +0200

    hero-section-responsive-fixed

--- frontend/src/pages/home/home.tsx
+++ frontend/src/pages/home/home.tsx
@@ -1,4 +1,4 @@
-// import { AsideFilter } from "./asideFilter/asideFilter";
+import { AsideFilter } from "./asideFilter/asideFilter";
 // import { CitiesGrid } from "./cities/cities";
 import { HeroSection } from "./heroSection/heroSection";
 import "./home.scss";
@@ -7,10 +7,10 @@ export function Home() {
   return (
     <>
       <HeroSection />
-      {/* <div className="flex w-full flex-1">
+      <div className="flex w-full flex-1">
         <AsideFilter />
-        <CitiesGrid />
-      </div> */}
+        {/* <CitiesGrid /> */}
+      </div>
     </>
   );
 }
