73983b520ed65d5f8fba388f6aaa57c90cf1c8da
massije <jedc.marti@gmail.com>
2024-05-20 14:46:08 +0200

    hero-section-responsive-fixed

--- frontend/src/pages/home/heroSection/heroSection.tsx
+++ frontend/src/pages/home/heroSection/heroSection.tsx
@@ -1,13 +1,14 @@
 import video from "@Assets/video.mp4";
 import "./heroSection.scss";
+// import { useState, useEffect } from "react";
 
 export const HeroSection = () => {
   return (
     <>
       <div className="hero-video-container">
-        <div className="curtain"></div>
+        <div className="curtain min-h-[780px] xl:h-screen"></div>
         <video
-          className="hero-video"
+          className="hero-video min-h-[780px] xl:h-screen"
           autoPlay
           loop
           muted
@@ -17,34 +18,36 @@ export const HeroSection = () => {
           <source src={video} type="video/mp4"></source>
           Your browser does not support HTML5 video.
         </video>
-      </div>
-      <div className="absolute top-0 flex flex-col items-start text-white mx-6 mt-44">
-        <h1 className="text-4xl font-bold sm:text-6xl">
-          <div className="mb-3 min-[348px]:hidden">Work</div>
-          <div className="mb-3 min-[348px]:hidden">Remote.</div>
-          <div className="mb-3 hidden min-[348px]:block">Work Remote.</div>
-          <div className="mb-3">Live Local.</div>
-          <div className="mb-3">Go Nomad!</div>
-        </h1>
-        <div className="divider"></div>
-        <section className="text-lg flex flex-col sm:text-xl">
-          <h2 className="font-bold mt-2 mb-8 text-xl sm:text-2xl">
-            Trusted and reliable data. Fresh and growing community.
-          </h2>
-          <div className="mb-4 sm:mb-8">
-            <b>Connect </b>
-            with like-minded peers through meetups & activities.
-          </div>
-          <div className="mb-4 sm:mb-8">
-            <b>Discover </b>
-            the ideal place to settle down.
-          </div>
-          <div className="mb-4 sm:mb-8">
-            <b>Track </b>
-            your journeys and show the world the places you've visited.
-          </div>
-          <button className="btn btn-accent mt-10">GO NOMAD</button>
-        </section>
+        <div className="absolute top-0 flex flex-col items-start text-white mx-6 mt-40 md:mx-12 xl:mx-56 xl:mt-56">
+          <h1 className="text-4xl font-bold sm:text-6xl">
+            <div className="mb-3 min-[348px]:hidden">Work</div>
+            <div className="mb-3 min-[348px]:hidden">Remote.</div>
+            <div className="mb-3 hidden min-[348px]:block">Work Remote.</div>
+            <div className="mb-3">Live Local.</div>
+            <div className="mb-3">Go Nomad!</div>
+          </h1>
+          <div className="divider"></div>
+          <section className="text-lg flex flex-col">
+            <h2 className="font-bold mt-2 mb-8 text-xl xl:text-lg">
+              Trusted and reliable data. Fresh and growing community.
+            </h2>
+            <div className="mb-4 md:mb-2 ">
+              <b>Connect </b>
+              with like-minded peers through meetups & activities.
+            </div>
+            <div className="mb-4 md:mb-2 ">
+              <b>Discover </b>
+              the ideal place to settle down.
+            </div>
+            <div className="mb-4 md:mb-2">
+              <b>Track </b>
+              your journeys and show the world the places you've visited.
+            </div>
+            <button className="btn btn-accent mt-10 md:w-[120px] xl:mt-6">
+              GO NOMAD
+            </button>
+          </section>
+        </div>
       </div>
     </>
   );
