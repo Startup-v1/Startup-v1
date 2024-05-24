0627f2f3ecae732928e42918b741df155450b7e3
massije <jedc.marti@gmail.com>
2024-05-24 14:57:05 +0200

    curtain-added

--- frontend/src/pages/home/asideFilter/asideFilter.tsx
+++ frontend/src/pages/home/asideFilter/asideFilter.tsx
@@ -7,21 +7,29 @@ import { FilterGroup } from "./filters/filterGroup";
 
 export function AsideFilter() {
   const { weather, where, other } = filterOptions;
-  const [isHidden, setIsHidden] = useState(true); // Comienza oculto por defecto
+  const [isHidden, setIsHidden] = useState(true);
+  const [isMedium, setIsMedium] = useState(window.innerWidth < 768);
+  const [isLarge, setIsLarge] = useState(window.innerWidth >= 1280);
   const filterRef = useRef<HTMLDivElement>(null);
 
-  const handleHideAsideFilter = () => {
-    setIsHidden(true);
+  const handleToggleAsideFilter = () => {
+    setIsHidden(!isHidden);
+  };
+
+  const handleResize = () => {
+    const width = window.innerWidth;
+    setIsMedium(width < 768);
+    setIsLarge(width >= 1280);
+    if (width >= 1280) {
+      setIsHidden(false);
+    } else {
+      setIsHidden(true); // Ensure hidden by default on smaller screens
+    }
   };
 
   useEffect(() => {
-    const handleResize = () => {
-      setIsHidden(window.innerWidth < 1280); // Ocultar cuando el ancho de la ventana sea menor que 1280px
-    };
-
-    handleResize(); // Verificar el ancho inicial en el montaje del componente
+    handleResize();
     window.addEventListener("resize", handleResize);
-
     return () => {
       window.removeEventListener("resize", handleResize);
     };
@@ -42,65 +50,97 @@ export function AsideFilter() {
     };
   }, []);
 
-  useEffect(() => {
-    const handleScroll = () => {
-      const filterElement = filterRef.current;
-      if (!filterElement) return;
-
-      const filterRect = filterElement.getBoundingClientRect();
-      const buttonElement = filterElement.querySelector(
-        ".applyButton"
-      ) as HTMLElement;
-
-      if (!buttonElement) return;
-
-      const buttonRect = buttonElement.getBoundingClientRect();
-      const windowHeight = window.innerHeight;
-
-      if (filterRect.bottom < windowHeight) {
-        filterElement.classList.add("sticky");
-        filterElement.style.bottom = `${windowHeight - buttonRect.bottom}px`;
-      } else {
-        filterElement.classList.remove("sticky");
-        filterElement.style.bottom = "";
-      }
-    };
-
-    window.addEventListener("scroll", handleScroll);
-
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
-
   return (
-    <div
-      ref={filterRef}
-      className={`w-full h-screen flex flex-col relative md:w-[360px] md:border md:border-r-gray transition-transform duration-500 ease-in-out ${
-        isHidden ? "transform -translate-x-full" : ""
-      }`}
-      style={{ position: "sticky", top: 0 }}
-    >
-      {/* Sticky-block */}
-      <FilterToolbar onHideAsideFilter={handleHideAsideFilter} />
-
-      {/* Barra-navegación personalizada */}
-      <div
-        className={`overflow-y-auto bg-white flex-1 ${
-          isHidden ? "hidden md:block" : "" // Ocultar en tamaños de pantalla pequeños, mostrar solo en md (o el breakpoint deseado)
-        }`}
-        style={{ position: "sticky", top: "50px" }}
-      >
-        <FilterGroup filterOptions={weather} title={"Weather"} />
-        <FilterGroup filterOptions={where} title="Where" size="px-2 py-3" />
-        <FilterPriceRange />
-        <FilterGroup filterOptions={other} title="Other" />
-      </div>
-
-      {/* Sticky-block */}
-      <div className={`applyButton ${isHidden ? "hidden md:block" : ""}`}>
-        <FilterApplyButton />
-      </div>
-    </div>
+    <>
+      {!isLarge && (
+        <div
+          className={`fixed inset-0 z-50 bg-black bg-opacity-50 transition-opacity duration-500 ease-in-out ${
+            isHidden
+              ? "opacity-0 pointer-events-none"
+              : "opacity-100 pointer-events-auto"
+          }`}
+          onClick={handleToggleAsideFilter}
+        ></div>
+      )}
+      {isMedium && (
+        <div
+          className={`fixed inset-0 z-50 transition-transform duration-500 ease-in-out ${
+            isHidden ? "transform -translate-x-full" : ""
+          }`}
+          onClick={handleToggleAsideFilter}
+        >
+          <div
+            ref={filterRef}
+            className="w-full h-full bg-white overflow-y-auto flex flex-col"
+            onClick={(e) => e.stopPropagation()}
+          >
+            <FilterToolbar onHideAsideFilter={handleToggleAsideFilter} />
+            <div className="flex-1">
+              <FilterGroup filterOptions={weather} title={"Weather"} />
+              <FilterGroup
+                filterOptions={where}
+                title="Where"
+                size="px-2 py-3"
+              />
+              <FilterPriceRange />
+              <FilterGroup filterOptions={other} title="Other" />
+            </div>
+            <div className="applyButton">
+              <FilterApplyButton />
+            </div>
+          </div>
+        </div>
+      )}
+      {!isMedium && !isLarge && (
+        <div
+          className={`fixed inset-0 z-50 transition-transform duration-500 ease-in-out ${
+            isHidden ? "transform -translate-x-full" : ""
+          }`}
+          onClick={handleToggleAsideFilter}
+        >
+          <div
+            ref={filterRef}
+            className="w-[360px] h-full bg-white overflow-y-auto flex flex-col"
+            onClick={(e) => e.stopPropagation()}
+          >
+            <FilterToolbar onHideAsideFilter={handleToggleAsideFilter} />
+            <div className="flex-1">
+              <FilterGroup filterOptions={weather} title={"Weather"} />
+              <FilterGroup
+                filterOptions={where}
+                title="Where"
+                size="px-2 py-3"
+              />
+              <FilterPriceRange />
+              <FilterGroup filterOptions={other} title="Other" />
+            </div>
+            <div className="applyButton">
+              <FilterApplyButton />
+            </div>
+          </div>
+        </div>
+      )}
+      {isLarge && (
+        <div
+          ref={filterRef}
+          className={`w-full h-screen flex flex-col relative md:w-[360px] md:border md:border-r-gray transition-transform duration-500 ease-in-out ${
+            isHidden ? "transform -translate-x-full" : ""
+          }`}
+          style={{ position: "sticky", top: 0 }}
+          onClick={handleToggleAsideFilter}
+        >
+          <FilterToolbar onHideAsideFilter={handleToggleAsideFilter} />
+          <div className="overflow-y-auto flex-1 bg-white">
+            <FilterGroup filterOptions={weather} title={"Weather"} />
+            <FilterGroup filterOptions={where} title="Where" size="px-2 py-3" />
+            <FilterPriceRange />
+            <FilterGroup filterOptions={other} title="Other" />
+          </div>
+          <div className="applyButton">
+            <FilterApplyButton />
+          </div>
+        </div>
+      )}
+    </>
   );
 }
